6346c46a8ff42d622dad2681a613c390502be2b9
Nina Kin <matikin9@gmail.com>
2024-09-06 00:58:18 -0700

    remove code

--- src/js/site-footer.js
+++ src/js/site-footer.js
@@ -41,34 +41,3 @@ function createMarkers(data) {
         let marker = L.marker([destination.lat, destination.lon]).addTo(map);
     });
 }
-
-
-// fetch('./data/categories.json')
-// .then((response) => response.json())
-// .then((json) => {
-//     json.forEach(category => {
-//         let path = './data/' + category
-
-//         fetch(path)
-//         .then((response2) => response2.json())
-//         .then((json2) => {
-//             json2.forEach(shop => {
-//                 let marker = L.marker([shop.location.lat, shop.location.lon]).addTo(map);
-                
-//                 let newElemName = document.createElement('h2');
-//                 newElemName.textContent = shop.name;
-
-//                 let newElemDesc = document.createElement('span');
-//                 newElemDesc.textContent = shop.description;
-                
-//                 let newElem = document.createElement('div');
-//                 newElem.appendChild(newElemName);
-//                 newElem.appendChild(newElemDesc);
-                
-//                 let list = document.querySelector('#list div');
-//                 list.appendChild(newElem);
-//             });
-            
-//         });
-//     });
-// });
